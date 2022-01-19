d8e6c0b534eb0debc240921dcc842e1f5a6a9a5b
Souhayl <souhayl.bendroh@edu.esiee-it.fr>
2022-01-19 16:03:29 +0100

    school_flutter temperature, dateAge, dateFriend [UPDATED]

--- lib/methodes/id_date_age_methodes.dart
+++ lib/methodes/id_date_age_methodes.dart
@@ -1,10 +1,19 @@
-class DateAgeMethodes {
-  DateTime dateAnniv = DateTime.parse("2022-01-19");
-  DateTime dateAjd = DateTime.now();
+import 'package:flutter/foundation.dart';
 
-  Duration diffAge = dateAnniv.difference(dateAjd);
+  final dateAnniv = DateTime(2019, 02, 22);
+  final dateAjd = DateTime.now();
+  final differenceJour = dateAjd.difference(dateAnniv).inDays;
+  final differenceMois = dateAjd.difference(dateAnniv).inDays ~/30;
+  final differenceHeure = dateAjd.difference(dateAnniv).inHours;
+
+
+  void main() {
+    if (kDebugMode) {
+      print(differenceMois);
+    }
+  }
 
 //print(diffAge.inDays); Affiche difference en jour
 //print(diffAge.inDays ~/30); Affiche difference en mois
 //print(diffAge.inHours); Affiche difference en heures
-}
+
--- lib/methodes/id_date_friend_methodes.dart
+++ lib/methodes/id_date_friend_methodes.dart
@@ -1,11 +1,14 @@
-class IdDateFriendMethodes {
-  DateTime picker1 = DateTime.parse("2022-01-19");
-  DateTime picker2 = DateTime.parse("2021-01-20");
+import 'package:flutter/foundation.dart';
 
-  Duration diffFriend = picker1.difference(picker2);
+final datePicker1 = DateTime(2019, 02, 22);
+final datePicker2 = DateTime(2029, 02, 22);
+final differenceJour = datePicker2.difference(datePicker1).inDays;
+final differenceMinute = datePicker2.difference(datePicker1).inMinutes;
+final differenceHeure = datePicker2.difference(datePicker1).inHours;
 
-//print(diffFriend.inDays); Affiche difference en jour
-//print(diffFriend.inDays ~/30); Affiche difference en mois
-//print(diffFriend.inDays ~/365); Affiche difference en annee
 
+void main() {
+  if (kDebugMode) {
+    print(differenceHeure);
+  }
 }
